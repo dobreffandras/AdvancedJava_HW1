0d93393a05300fc37f56d23b8da1c02f7437d70d
András Dobreff <dobreffandras@gmail.com>
2016-04-01 23:42:51 +0200

    Added writer to write to file

--- JavaApplication1/src/reactive/JavaApplication1.java
+++ JavaApplication1/src/reactive/JavaApplication1.java
@@ -5,6 +5,10 @@
  */
 package reactive;
 
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+
 /**
  *
  * @author Andris
@@ -14,14 +18,18 @@ public class JavaApplication1 {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException, UnsupportedEncodingException {
+        
+        PrintWriter writer = new PrintWriter("output.txt", "UTF-8");
+        
         ConsoleReaderSignal reader = ConsoleReaderSignal.createConsoleReaderSignal();
         Long startupTime = System.currentTimeMillis();
         TimerSignal timer = Time.every(1, TimeUnitsEnum.SECOND, null);
         timer.setAction(() -> {
-            System.out.println("Last line on input: "+reader.getValue()+
-                    ", time elapsed: "+(System.currentTimeMillis()-startupTime)/1000+"s");
+            writer.write("Last line on input: "+reader.getValue()+ 
+                    ", time elapsed: "+(System.currentTimeMillis()-startupTime)/1000+"s"+
+                    System.lineSeparator());
+            writer.flush();
         });
     }
-    
 }
