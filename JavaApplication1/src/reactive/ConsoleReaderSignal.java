6b12dae1696261a9c17f062d5fb2c7f5e4e81df5
András Dobreff <dobreffandras@gmail.com>
2016-04-01 22:45:26 +0200

    Added ConsoleReaderSignal

---
+++ JavaApplication1/src/reactive/ConsoleReaderSignal.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package reactive;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Andris
+ */
+public class ConsoleReaderSignal extends Signal<String>{
+    
+    protected ConsoleReaderSignal() {
+        super(null);
+        
+        new Thread(() -> {
+            Scanner scan = new Scanner(System.in);
+            while (scan.hasNext()) {
+                String input = scan.nextLine();
+                this.value = input;
+            }
+        }).start();
+        
+    }
+    
+    public static ConsoleReaderSignal createConsoleReaderSignal(){
+        return new ConsoleReaderSignal();
+    }
+    
+}
